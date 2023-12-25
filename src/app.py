aa8872cb1a942828e6bc7fbe554557167dbc8e30
iceproc <iceproc1337@proton.me>
2023-12-26 03:03:02 +0800

    remove image_model for now.

--- src/app.py
+++ src/app.py
@@ -40,12 +40,6 @@ text_generation_config = {
     "top_k": 1,
     "max_output_tokens": 512,
 }
-image_generation_config = {
-    "temperature": 0.4,
-    "top_p": 1,
-    "top_k": 32,
-    "max_output_tokens": 512,
-}
 safety_settings = [
     {"category": "HARM_CATEGORY_HARASSMENT", "threshold": "BLOCK_MEDIUM_AND_ABOVE"},
     {"category": "HARM_CATEGORY_HATE_SPEECH", "threshold": "BLOCK_MEDIUM_AND_ABOVE"},
@@ -61,11 +55,6 @@ safety_settings = [
 text_model = genai.GenerativeModel(
     model_name="gemini-pro",
     generation_config=text_generation_config,
-    safety_settings=safety_settings,
-)
-image_model = genai.GenerativeModel(
-    model_name="gemini-pro-vision",
-    generation_config=image_generation_config,
     safety_settings=safety_settings,
 )
 
