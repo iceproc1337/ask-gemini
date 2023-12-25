7680380f37ef6dd93db060c40ace9f5134250493
iceproc <iceproc1337@proton.me>
2023-12-26 02:27:02 +0800

    refractored code

--- templates/main.js
+++ templates/main.js
@@ -1,81 +1,62 @@
-function domAddUserInput(text) {
-    var chatContainer = document.querySelector('.chat-container');
-    var chatBubble = document.createElement('div');
+function addChatBubble(text, isUser) {
+    const chatContainer = document.querySelector('.chat-container');
+    const chatBubble = document.createElement('div');
     chatBubble.classList.add('chat-bubble');
-    chatBubble.classList.add('chat-bubble-right');
+    chatBubble.classList.add(isUser ? 'chat-bubble-right' : 'chat-bubble-left');
     chatBubble.innerText = text;
     chatContainer.appendChild(chatBubble);
 
     // Check if the user is already scrolled to the bottom
     if (chatContainer.scrollTop + chatContainer.clientHeight >= chatContainer.scrollHeight) {
         chatContainer.scrollTop = chatContainer.scrollHeight;
-    } {
-        chatContainer.scrollTop = chatContainer.scrollHeight;
-    }
-}
-
-function domAddGeminiOutput(text) {
-    var chatContainer = document.querySelector('.chat-container');
-    var chatBubble = document.createElement('div');
-    chatBubble.classList.add('chat-bubble');
-    chatBubble.classList.add('chat-bubble-left');
-
-    chatBubble.innerHTML = marked.parse(text);
-    chatContainer.appendChild(chatBubble);
-
-    // Check if the user is already scrolled to the bottom
-    if (chatContainer.scrollTop + chatContainer.clientHeight >= chatContainer.scrollHeight) {
-        chatContainer.scrollTop = chatContainer.scrollHeight;
-    } {
+    } else {
         chatContainer.scrollTop = chatContainer.scrollHeight;
     }
 }
 
 function toggleMenu() {
-    var menuItems = document.querySelector('.menu-items');
+    const menuItems = document.querySelector('.menu-items');
     menuItems.classList.toggle('show');
 }
 
-function onChatContainerClick(event) {
+function copyCodeToClipboard(event) {
     if (event.target.matches('code')) {
-        // Copy the text inside the code block to the clipboard
-        navigator.clipboard.writeText(event.target.textContent);
+        const codeText = event.target.textContent;
+        navigator.clipboard.writeText(codeText);
     }
 }
 
-function onDomContentLoaded() {
-    var inputField = document.getElementById('input-field');
-    var form = document.getElementById('chat-form');
-    var menuButton = document.querySelector('.menu-button');
-    var chatContainer = document.querySelector('.chat-container');
+function handleFormSubmit(event) {
+    event.preventDefault(); // prevent form submission
 
-    form.addEventListener('submit', function (e) {
-        e.preventDefault(); // prevent form submission
-        let text = inputField.value;
-        console.log(text);
+    const inputField = document.getElementById('input-field');
+    const text = inputField.value;
+    console.log(text);
 
-        domAddUserInput(text.toString());
+    addChatBubble(text.toString(), true);
 
-        // Send HTTP POST request to "/ask-gemini" with parameter "query" of value text.toString()
-        var xhr = new XMLHttpRequest();
-        xhr.open("POST", "/ask-gemini", true);
-        xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
-        xhr.onreadystatechange = function () {
-            if (xhr.readyState === 4 && xhr.status === 200) {
-                // Handle the response here
-                console.log(xhr.responseText);
-                domAddGeminiOutput(xhr.responseText);
-            }
-        };
-        xhr.send("query=" + encodeURIComponent(text.toString()));
+    // Send HTTP POST request to "/ask-gemini" with parameter "query" of value text.toString()
+    const xhr = new XMLHttpRequest();
+    xhr.open("POST", "/ask-gemini", true);
+    xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+    xhr.onreadystatechange = function () {
+        if (xhr.readyState === 4 && xhr.status === 200) {
+            // Handle the response here
+            console.log(xhr.responseText);
+            addChatBubble(xhr.responseText, false);
+        }
+    };
+    xhr.send("message=" + encodeURIComponent(text.toString()));
 
-        inputField.value = ''; // clear the input field
-    });
-
-    // Show menu items when menu button is clicked
-    menuButton.addEventListener('click', toggleMenu);
-
-    chatContainer.addEventListener('click', onChatContainerClick);
+    inputField.value = ''; // clear the input field
 }
 
-document.addEventListener('DOMContentLoaded', onDomContentLoaded);
+document.addEventListener('DOMContentLoaded', function () {
+    const form = document.getElementById('chat-form');
+    const menuButton = document.querySelector('.menu-button');
+    const chatContainer = document.querySelector('.chat-container');
+
+    menuButton.addEventListener('click', toggleMenu);
+    chatContainer.addEventListener('click', copyCodeToClipboard);
+    form.addEventListener('submit', handleFormSubmit);
+});
